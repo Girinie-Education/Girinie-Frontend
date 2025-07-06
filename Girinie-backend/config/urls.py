aa6e26e30e9d6476ddf14cec36377955d2404231
Kimseongju7 <rlatj777@naver.com>
2025-07-07 00:06:08 +0900

    add child get, put, post, delete api

--- Girinie-backend/config/urls.py
+++ Girinie-backend/config/urls.py
@@ -19,8 +19,11 @@ from django.urls import path, include
 from rest_framework import permissions
 from drf_yasg.views import get_schema_view
 from drf_yasg import openapi
+from django.conf import settings
+from django.conf.urls.static import static
 
 
+# swagger
 schema_view = get_schema_view(
     openapi.Info(
         title="Girinie API",
@@ -35,7 +38,12 @@ schema_view = get_schema_view(
 urlpatterns = [
     path('admin/', admin.site.urls),
     path('api/v1/parent_users/', include('parent_users.urls')),
+    path('api/v1/child_users/', include('child_users.urls')),
     # Swagger URL
     path('swagger/', schema_view.with_ui('swagger', cache_timeout=0), name='schema-swagger-ui'),
     path('redoc/', schema_view.with_ui('redoc', cache_timeout=0), name='schema-redoc'),
 ]
+
+# 미디어 파일 서빙 (개발 환경에서만)
+if settings.DEBUG:
+    urlpatterns += static(settings.MEDIA_URL, document_root=settings.MEDIA_ROOT)
